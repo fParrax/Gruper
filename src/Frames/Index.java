dac3e84c00a97320a344078b373dfdadc84ad7dd
fParrax <franklinlpl@gmail.com>
2023-10-30 07:23:16 -0400

    d

--- src/Frames/Index.java
+++ src/Frames/Index.java
@@ -24,7 +24,7 @@ private void iniciar(){
     vwBienvenido = new viewBienvenido();
     scrollPane.setViewportView(vwBienvenido);
     
-    vwAgencias = new viewAgencias();
+   // vwAgencias = new viewAgencias();
 }
     
     @SuppressWarnings("unchecked")
@@ -37,6 +37,7 @@ private void iniciar(){
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu3 = new javax.swing.JMenu();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
 
@@ -77,6 +78,15 @@ private void iniciar(){
         jMenuBar1.add(jMenu3);
 
         jMenu1.setText("Resultados");
+
+        jMenuItem1.setText("Ingresar Resultado");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Ventas");
@@ -101,6 +111,10 @@ private void iniciar(){
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+     new newResultado().setVisible(true);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -143,6 +157,7 @@ private void iniciar(){
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel panelCentral;
     private javax.swing.JPanel panelSuperior;
     private javax.swing.JScrollPane scrollPane;
